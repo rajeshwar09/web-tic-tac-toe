3ef910d4e4071eb37b833d64042607b93b706db9
Raje <rajeshwar.work09@gmail.com>
2025-02-22 19:36:47 +0530

    feat: added editable player field component

---
+++ src/components/player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing); //  Always give latest updated state
+    // setIsEditing(!isEditing) // => schedules a state update to true (may/maynot give updated state)
+  };
+
+  const handleOnChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editableNameField = <span className="player-name">{playerName}</span>;
+  let btnLabel = "Edit";
+
+  if (isEditing) {
+    editableNameField = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleOnChange}
+      />
+    );
+    btnLabel = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editableNameField}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnLabel}</button>
+    </li>
+  );
+};
+
+export default Player;
